d78d2ae9a201ef02da22fce31b2eed6d0be46f53
Andrew <Andreiks@inbox.ru>
2022-12-27 20:38:45 +0600

    added StaticFileReader

---
+++ MagmaTestServer/StaticFileReader.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace MagmaTestServer
+{
+    public static class StaticFileReader
+    {
+        public static JObject ReadTheJsonFile()
+        {
+            using (StreamReader file = System.IO.File.OpenText("data.json"))
+            using (JsonTextReader reader = new JsonTextReader(file))
+            {
+                return (JObject)JToken.ReadFrom(reader);
+            }
+        }
+    }
+}
