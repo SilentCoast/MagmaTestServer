60663b13a1d5e28866deaca34372c39fb35df140
User <User@DESKTOP-MO5GFUN>
2022-12-27 20:13:34 +0600

    Add project files.

---
+++ MagmaTestServer/Controllers/allDataController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MagmaTestServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class allDataController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            using (StreamReader file = System.IO.File.OpenText("data.json"))
+            using(JsonTextReader reader = new JsonTextReader(file))
+            {
+                return ((JObject)JToken.ReadFrom(reader)).ToString();
+            }
+               
+        }
+
+        
+    }
+}
