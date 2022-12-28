b4b46f77057815be71bbe2ef4c1395293add45ef
Andrew <Andreiks@inbox.ru>
2022-12-28 19:32:36 +0600

    serviceInfo GET

---
+++ MagmaTestServer/Classes/ServiceInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace MagmaTestServer.Classes
+{
+    public class ServiceInfo
+    {
+        public ServiceInfo() { }
+        public string AppName { get; set; }
+        public string Version { get; set; }
+        public DateTime DateUtc { get; set; }
+    }
+}
---
+++ MagmaTestServer/Controllers/serviceController.cs
@@ -0,0 +1,26 @@
+﻿using MagmaTestServer.Classes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagmaTestServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class serviceController : ControllerBase
+    {
+        [HttpGet]
+        [Route("serviceInfo")]
+        public ServiceInfo GetServiceInfo()
+        {
+            ServiceInfo serviceInfo = new ServiceInfo();
+            serviceInfo.AppName = System.Reflection.Assembly.GetExecutingAssembly().FullName;
+
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            System.Diagnostics.FileVersionInfo fvi = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+
+            serviceInfo.Version = fvi.FileVersion;
+            serviceInfo.DateUtc = DateTime.UtcNow;
+            return serviceInfo;
+        }
+    }
+}
